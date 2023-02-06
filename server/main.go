7edfac4d278c32e923033f1f458237ebb066f7f6
zhanghaisheng <zhanghaisheng@qimao.com>
2023-02-06 16:44:45 +0800

    简化代码

--- server/main.go
+++ server/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/go-kratos/kratos/v2/transport/http"
 	"github.com/hisheng/kratos-http/api"
 	"github.com/hisheng/kratos-http/service"
 	"log"
 	"net"
 )
-
-var h = func(w http.ResponseWriter, r *http.Request) {
-	_ = json.NewEncoder(w).Encode("s")
-}
 
 func main() {
 	ln, err := net.Listen("tcp", ":8080")
